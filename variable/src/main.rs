d0c83691d6ab82f52c972805956820cac9ba26d8
Hbbolaji <bolaji1050@gmail.com>
2026-04-27 15:01:06 +0100

    implement functions in rust

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -9,9 +9,24 @@ fn main() {
     println!("i8 limits: {}, {}", i8::MIN, i8::MAX);
     println!("i16 limits: {}, {}", i16::MIN, i16::MAX);
     println!("u8 limits: {}, {}", u8::MIN, u8::MAX);
-    println!("f32 limits: {}, {}", f32::MIN, f32::MAX);
-    println!("f64 limits: {}, {}", f64::MIN, f64::MAX);
+    println!("f32 Digits: {}", f32::DIGITS);
+    println!("f64 Digits: {}", f64::DIGITS);
 
     let hex = 5u8;
+    let base_8 = 0o764;
+    let bin = 0b1101_1101;
     let ascii_en = b'A';
+
+    show_number(bin);
+    show_number(base_8);
+    println!("sum of {} and {} = {}", hex, ascii_en, add_number(hex, ascii_en));
+}
+
+fn add_number(a: u8, b: u8) -> u8 {
+    let sum = a + b;
+    return sum;
+}
+
+fn show_number(num: i32) -> () {
+    println!("Number is {}", num);
 }
