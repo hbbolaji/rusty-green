0e8c8a20523ee0025b1dc3f080bda4b2a5d9a9df
Hbbolaji <bolaji1050@gmail.com>
2026-04-29 19:54:08 +0100

    implemented error handling with thiserror and updated dependencies

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,27 @@
+use thiserror;
+
+#[derive(thiserror::Error, Debug)]
+
+enum QuantityError {
+    #[error("Flowrate is too large")]
+    TooLarge,
+    #[error("Flowrate is too small")]
+    TooSmall,
+    #[error("Flowrate cannot be negative")]
+    Negative,
+}
+
+fn set_flowrate(flowrate: f64) -> Result<(), QuantityError> {
+    if flowrate < 0.0 {
+        return Err(QuantityError::Negative);
+    } else if flowrate < 0.1 {
+        return Err(QuantityError::TooSmall);
+    } else if flowrate > 100.0 {
+        return Err(QuantityError::TooLarge);
+    }
+    Ok(())
+}
+
 fn func_that_panics(num: u8) {
     if num > 5 {
         panic!("I can't handle numbers that large");
@@ -13,25 +37,30 @@ fn funct_that_returns_result(num: u8) -> Result<u8, String> {
 }
 
 fn main() {
-    // func_that_panics(3);
+    func_that_panics(3);
     // func_that_panics(10);
     // println!("150 * 2 = {}", funct_that_returns_result(150).expect("Function panic at 150"))
 
     // match
-    // let double_num = match funct_that_returns_result(150) {
-    //     Ok(result) => result,
-    //     Err(e) => {
-    //         println!("There was an error: {}", e);
-    //         println!("Defaulting to original number: 150");
-    //         150
-    //     }
-    // };
+    let double_num = match funct_that_returns_result(150) {
+        Ok(result) => result,
+        Err(e) => {
+            println!("There was an error: {}", e);
+            println!("Defaulting to original number: 150");
+            150
+        }
+    };
 
-    // println!("the resulting number is {}", double_num);
+    println!("the resulting number is {}", double_num);
 
     if let Ok(value) = funct_that_returns_result(150) {
         println!("The resulting number is {}", value);
     } else {
         println!("Could not double number")
+    }
+
+    match set_flowrate(50.0) {
+        Ok(_) => println!(""),
+        Err(e) => println!("{:?}", e)
     }
 }
