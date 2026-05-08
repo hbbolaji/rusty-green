431ce5af1c15df3af1cff0c81bdbd3a826ddada9
Hbbolaji <bolaji1050@gmail.com>
2026-05-08 21:06:10 +0100

    enhance CSV reading with comprehensive error handling

--- custom_error_handling/src/csvreader.rs
+++ custom_error_handling/src/csvreader.rs
@@ -1,9 +1,13 @@
 use std::{
+    error::Error,
+    fmt::Display,
     fs::OpenOptions,
-    io::{BufRead, BufReader},
+    io::{self, BufRead, BufReader},
     path::Path,
     str::FromStr,
 };
+
+type Result<T> = std::result::Result<T, CsvError>;
 
 #[derive(Debug)]
 pub struct CsvData<T: Copy + Default + FromStr> {
@@ -11,45 +15,96 @@ pub struct CsvData<T: Copy + Default + FromStr> {
     pub data: Vec<Vec<T>>,
 }
 
-pub fn read_csv<T: Copy + Default + FromStr>(filename: &str) -> CsvData<T> {
-    let lines = read_to_lines(filename);
+#[derive(Debug)]
+pub struct CsvLineLen {
+    pub line_num: usize,
+    pub num_entries: usize,
+}
+
+#[derive(Debug)]
+pub enum CsvError {
+    FileNonExistent,
+    CouldNotOpenFile(io::Error),
+    CouldNotParseLine(Box<dyn Error>),
+    FileIsEmpty,
+    CouldNotParseValue(String),
+    LineTooShort(CsvLineLen),
+    LineTooLong(CsvLineLen),
+}
+
+impl From<io::Error> for CsvError {
+    fn from(value: io::Error) -> Self {
+        Self::CouldNotOpenFile(value)
+    }
+}
+
+impl Display for CsvError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for CsvError {}
+
+pub fn read_csv<T: Copy + Default + FromStr>(filename: &str) -> Result<CsvData<T>> {
+    let lines = read_to_lines(filename)?;
 
     if lines.is_empty() {
-        return CsvData {
-            header: vec![],
-            data: vec![],
-        };
+        return Err(CsvError::FileIsEmpty);
     }
 
     let header: Vec<String> = lines[0].split(",").map(|s| s.into()).collect();
     let mut data: Vec<Vec<T>> = Vec::with_capacity(lines.len() - 1);
 
     for i in 1..lines.len() {
-        let entries: Vec<T> = lines[i]
+        let entries: Vec<Result<T>> = lines[i]
             .split(",")
-            .map(|e| e.parse::<T>())
-            .map(|e| e.unwrap_or_default())
+            .map(|e| {
+                let res = e.parse::<T>();
+                res.map_err(|_| CsvError::CouldNotParseLine(e.into()))
+            })
             .collect();
-        if entries.len() >= header.len() {
-            data.push(entries[0..header.len()].into());
+
+        let entries: Vec<T> = entries.into_iter().collect::<Result<_>>()?;
+        if entries.len() == header.len() {
+            data.push(entries);
+        } else if entries.len() > header.len() {
+            return Err(CsvError::LineTooLong(CsvLineLen {
+                line_num: i,
+                num_entries: entries.len(),
+            }));
+        } else {
+            return Err(CsvError::LineTooShort(CsvLineLen {
+                line_num: i,
+                num_entries: entries.len(),
+            }));
         }
     }
 
-    return CsvData { header, data };
+    return Ok(CsvData { header, data });
 }
 
-fn read_to_lines(filename: &str) -> Vec<String> {
+fn read_to_lines(filename: &str) -> Result<Vec<String>> {
     let path = Path::new(filename);
     if !path.exists() {
-        return vec![];
+        return Err(CsvError::FileNonExistent);
     }
 
-    let file_res = OpenOptions::new().read(true).open(path);
-    match file_res {
-        Ok(file) => {
-            let lines = BufReader::new(file).lines();
-            lines.filter(|l| l.is_ok()).map(|l| l.unwrap()).collect()
-        }
-        Err(_) => vec![],
-    }
+    let file = OpenOptions::new().read(true).open(path)?;
+    let lines: Vec<_> = BufReader::new(file).lines().collect();
+    lines
+        .into_iter()
+        .map(|line| line.map_err(|e| CsvError::CouldNotParseLine(Box::new(e))))
+        .collect()
+
+    // match file_res {
+    //     Ok(file) => {
+    //         let lines: Vec<_> = BufReader::new(file).lines().collect();
+    //         lines
+    //             .into_iter()
+    //             .map(|line| line.map_err(|e| CsvError::CouldNotParseLine(Box::new(e))))
+    //             .collect()
+    //     }
+    //     Err(e) => Err(CsvError::CouldNotOpenFile(e)),
+    // }
 }
--- custom_error_handling/src/main.rs
+++ custom_error_handling/src/main.rs
@@ -1,6 +1,6 @@
-pub mod csvreader;
-use crate::csvreader::read_csv;
+use crate::csvreader::{CsvData, read_csv};
+mod csvreader;
 fn main() {
-    let csv_data: csvreader::CsvData<i32> = read_csv("input.csv");
+    let csv_data: CsvData<i32> = read_csv("input.csv").unwrap();
     println!("{:?}", csv_data);
 }
