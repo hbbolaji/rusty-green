7700fa594810864dfa6e65bd4e50c9956137ab0f
Hbbolaji <bolaji1050@gmail.com>
2026-05-09 01:57:31 +0100

    implemented pattern matching

--- revision_1/src/main.rs
+++ revision_1/src/main.rs
@@ -1,4 +1,4 @@
-use crate::{closure::test_closures, pattern_matching::test_match_int};
+use crate::{closure::test_closures, pattern_matching::{test_match_array, test_match_int}};
 
 pub mod closure;
 pub mod pattern_matching;
@@ -6,4 +6,5 @@ pub mod pattern_matching;
 fn main() {
     test_closures();
     test_match_int();
+    test_match_array();
 }
--- revision_1/src/pattern_matching.rs
+++ revision_1/src/pattern_matching.rs
@@ -7,3 +7,14 @@ pub fn test_match_int () {
       _ => println!("old as fuck")
   }
 }
+
+pub fn test_match_array() {
+  let prices: [u32; 4] = [30000, 50000, 90000, 120000];
+
+  match &prices[0..=1] {
+    [30000, 50000] => println!("You have some reasonable priced cars"),
+    [50000, 90000] => println!("not too bad"),
+    [90001, 120000] => println!("I need to work hard"),
+    _ => println!("we don't want")
+  }
+}
