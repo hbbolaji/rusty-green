fa5b0256b5f051befdc0f917e8f6c4b51f3e1667
Hbbolaji <bolaji1050@gmail.com>
2026-05-16 20:49:58 +0100

    implemented Default trait

---
+++ revision_1/src/mydefault.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct LastName(String);
+
+impl Default for LastName {
+  fn default() -> Self {
+      Self("Bello".into())
+  }
+}
+
+#[derive(Debug)]
+struct Person {
+  firstname: String,
+  lastname: LastName,
+  age: u8,
+  location: String
+}
+
+impl Default for Person {
+  fn default() -> Self {
+      Self {
+        firstname: "Hashim".to_string(),
+        lastname: LastName::default(),
+        age: 28,
+        location: "Lagos, Nigeria".to_string()
+      }
+  }
+}
+
+pub fn test_default_impl() {
+  let mut person_1 = Person::default();
+  let mut person_2 = Person {
+    firstname: "Ibrahim".into(),
+    age: 25,
+    ..Person::default()
+  };
+
+  // let mut person_3 = 
+  person_1.location = "London, UK".into();
+  person_2.location = "Zanzibar, TZ".into();
+  println!("{:?}", person_1.lastname.0);
+  println!("{:?}", person_2);
+}
